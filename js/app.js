0c6991b6de07a0d51f7e46e8b4eb341caa15be61
MikeKuhl <38599344+MikeKuhl@users.noreply.github.com>
2021-11-04 12:15:27 -0700

    fixing merg conflict

--- js/app.js
+++ js/app.js
@@ -19,59 +19,134 @@ let hours = [
   "7pm",
   "8pm",
 ];
-let table = document.createElement('table');
+let stores = [];
+let cookieTable = document.getElementById('cookieTable');
 let thead = document.createElement('thead');
 let tbody = document.createElement('tbody');
 
 
 
-function Store(name, minCustomer, maxCustomer, cookiesHrly, hours) {
+function Store(name, minCustomer, maxCustomer, avgCookie) {
+
   this.name = name;
   this.minCustomer = minCustomer;
   this.maxCustomer = maxCustomer;
-  this.cookiesHrly = cookiesHrly;
-  this.hours = hours;
+  this.avgCookie = avgCookie;
 
-  this.customers = 0;
+
+  this.totalCustomersPerHour = [];
+  this.cookiesPerHour = []
   this.totalSale = 0;
-  this.hrlyCust = 0
-  this.randCust();
-  this.counter();
-  this.perHrSale();
+
+  this.customersPerHour();
+  this.cookiesPerSale();
+  stores.push(this);
+
+
 }
-//generates a random number of sales/customers per day
-Store.prototype.randCust = function()  {
-  let range = this.maxCustomer - this.minCustomer + 1;
-  this.customers = Math.floor(Math.random() * range) + this.minCustomer;
-}
+
 //returns total number of cookies sold every hour
-Store.prototype.counter =  function() {
-  let total = this.totalSale[0]
-  for (let i = 1; i < this.totalSale.length; i +=1) {
-   total = total + this.totalSale[i];
+Store.prototype.customersPerHour =  function() {
+  for (let i = 0; i < hours.length; i+= 1) {
+    let hourlyCustomers = Math.floor(Math.random() * (this.maxCustomers - this.minCustomers + 1)) + this.minCustomers;
+    this.totalCustomersPerHour.push(hourlyCustomers);
+  }
+}
+//this should return a sale per hour based on the random customersPerHour per hour
+Store.prototype.cookiesPerSale = function () {
+  for (let i = 0; i < hours.length; i+= 1) {
+    let hourlySales = Math.ceil(this.totalCustomersPerHour[i] * this.avgCookie);
+    this.cookiesPerHour.push(hourlySales);
+    this.totalSale += hourlySales;
   }
 
 }
-//this should return a sale per hour based on the random customers per hour
-Store.prototype.perHrSale = function () {
-  for (let i = 0; i < this.hours.length; i +=1 ) {
-  let range = this.randCust(this.minCustomer, this.maxCustomer);
 
-    this.cookiesHrly = Math.round(this.cookiesHrly * range);
+
+
+
+Store.prototype.render = function () {
+let row = document.createElement('tr');
+let tabData = document.createElement('td');
+tabData.textContent = this.name;
+row.appendChild(tabData);
+for (let i = 0; i < this.cookiesPerHour.length; i +=1) {
+  let cell = document.createElement('td');
+  cell.textContent = this.cookiesPerHour[i];
+  row.appendChild(cell);
+};
+let total = document.createElement('td');
+total.textContent =  this.totalSale;
+row.appendChild(total);
+cookieTable.appendChild(row);
+};
+
+
+
+function renderHeader() {
+  let tableRow = document.createElement('tr');
+  let thElem =  document.createElement('th');
+  thElem.textContent = null;
+  cookieTable.appendChild(tableRow);
+  tableRow.appendChild(thElem);
+  for (let i = 0; i < hours.length; i+=1) {
+    thElem = document.createElement('th');
+    thElem.textContent = hours[i];
+    tableRow.appendChild(thElem);
   }
-  return this.cookiesHrly;
+  thElem = document.createElement('th');
+  thElem.textContent = 'Store Total';
+  tableRow.appendChild(thElem);
+  cookieTable.appendChild(tableRow)
+};
+
+function renderStores() {
+  for (let i = 0; i < stores.length; i+=1) {
+    stores[i].render();
+  }
+};
+
+function renderFooterRow() {
+  let tableRow = document.createElement('tr');
+  tableRow.textContent = 'Totals';
+  cookieTable.appendChild(tableRow);
+  let total = 0;
+  for (let hoursIndex = 0; hoursIndex < hours.length; hoursIndex +=1) {
+    let hourlyTotal = 0;
+    for (let j = 0; j < stores.length; j+=1) {
+      hourlyTotal = hourlyTotal + stores[j].cookiesPerHour[hoursIndex];
+      total += stores[j].cookiesPerHour[hoursIndex];
+    }
+    var tdData = document.createElement('td');
+    tdData.textContent = hourlyTotal;
+    tableRow.appendChild(tdData);
+  }
+  tdData = document.createElement('td');
+  tdData.textContent = total;
+  tableRow.appendChild(tdData);
 }
 
-let paris = new Store("Paris", 20, 38, 2.3, hours);
-let lima = new Store("Lima", 2, 16, 4.6, hours);
-let seattle = new Store("Seattle", 2, 16, 4.6, hours);
-let tokyo = new Store("Tokyo", 2, 16, 4.6, hours);
-let dubai = new Store("Dubai", 2, 16, 4.6, hours);
+// constructs new stores
+let paris = new Store("Paris", 20, 38, 2.3);
+let lima = new Store("Lima", 2, 16, 4.6);
+let seattle = new Store("Seattle", 2, 16, 4.6);
+let tokyo = new Store("Tokyo", 2, 16, 4.6);
+let dubai = new Store("Dubai", 2, 16, 4.6);
 
 
+renderHeader();
+renderStores();
+renderFooterRow();
 
-const tableContainer = document.getElementById('StoreData').appendChild(table);
-
+//theme
 switchElement.addEventListener("click", () => {
   document.body.classList.toggle("dark");
 });
+
+
+//https://www.delftstack.com/howto/javascript/create-table-javascript/
+//https://www.delftstack.com/howto/javascript/create-table-javascript/
+//https://stackoverflow.com/questions/8302166/dynamic-creation-of-table-with-dom
+//https://www.tutorialrepublic.com/html-tutorial/html-tables.php
+//https://www.codeproject.com/Articles/1036671/Creating-HTML-Tables-with-JavaScript-DOM-Methods
+//https://www.py4u.net/discuss/277466
